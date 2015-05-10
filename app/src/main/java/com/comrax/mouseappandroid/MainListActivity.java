85046d31ea8299045b8a09e365e390cdfa268bc8
Bez <bez@comrax.com>
2015-05-10 18:11:49 +0300

    master

---
+++ app/src/main/java/com/comrax/mouseappandroid/MainListActivity.java
@@ -0,0 +1,19 @@
+package com.comrax.mouseappandroid;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by bez on 10/05/2015.
+ */
+public class MainListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_list);
+
+
+    }
+
+}
