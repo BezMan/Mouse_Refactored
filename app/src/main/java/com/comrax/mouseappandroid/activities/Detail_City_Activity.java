f7ef4a32ffc4296d5caa30c4291a7f45aa99b8f3
Bez <bez@comrax.com>
2015-06-03 17:53:42 +0300

    great progress with expandable list. lets continue.

--- app/src/main/java/com/comrax/mouseappandroid/activities/Detail_City_Activity.java
+++ app/src/main/java/com/comrax/mouseappandroid/activities/Detail_City_Activity.java
@@ -1,15 +1,20 @@
 package com.comrax.mouseappandroid.activities;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.widget.ListView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ExpandableListView;
 import android.widget.TextView;
 
 import com.comrax.mouseappandroid.R;
+import com.comrax.mouseappandroid.adapters.AnimatedExpandableListView;
 import com.comrax.mouseappandroid.adapters.DetailsListAdapter;
 import com.comrax.mouseappandroid.database.DBConstants;
 import com.comrax.mouseappandroid.database.DBTools;
@@ -31,6 +36,10 @@ public class Detail_City_Activity extends MyDrawerLayoutActivity {
     DetailsListAdapter detailsListAdapter;
     public String CITY_FOLDER_NAME, CITY_UPDATE_DATE, cityId;
     DBTools dbTools = new DBTools(this);
+
+    private AnimatedExpandableListView listView;
+    private ExampleAdapter adapter;
+
     @Override
     protected int getLayoutResourceId() {
         return R.layout.activity_detail_city;
@@ -44,6 +53,58 @@ public class Detail_City_Activity extends MyDrawerLayoutActivity {
         setTitle();
 
     }
+
+
+
+
+
+    private void setExpandableList(){
+
+        List<GroupItem> items = new ArrayList<GroupItem>();
+
+        // Populate our list with groups and it's children
+        for(int i = 1; i < 100; i++) {
+            GroupItem item = new GroupItem();
+
+            item.title = "Group " + i;
+
+            for(int j = 0; j < i; j++) {
+                ChildItem child = new ChildItem();
+                child.title = "Awesome item " + j;
+                child.hint = "Too awesome";
+
+                item.items.add(child);
+            }
+
+            items.add(item);
+        }
+
+        adapter = new ExampleAdapter(this);
+        adapter.setData(items);
+
+        listView = (AnimatedExpandableListView) findViewById(R.id.details_list);
+        listView.setAdapter(adapter);
+
+        // In order to show animations, we need to use a custom click handler
+        // for our ExpandableListView.
+        listView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
+                // We call collapseGroupWithAnimation(int) and
+                // expandGroupWithAnimation(int) to animate group
+                // expansion/collapse.
+                if (listView.isGroupExpanded(groupPosition)) {
+                    listView.collapseGroupWithAnimation(groupPosition);
+                } else {
+                    listView.expandGroupWithAnimation(groupPosition);
+                }
+                return true;
+            }
+
+        });
+    }
+
 
     private void setTitle() {
         String title = dbTools.getData(DBConstants.CITY_TABLE_NAME, DBConstants.hebrewName, DBConstants.cityId, cityId);
@@ -64,8 +125,9 @@ public class Detail_City_Activity extends MyDrawerLayoutActivity {
 
         addPagerData(jsonData);
 
-        setListItems();
+//        setListItems();
 
+        setExpandableList();
 
     }
 
@@ -103,7 +165,7 @@ public class Detail_City_Activity extends MyDrawerLayoutActivity {
                 myDetailsArray.add(listItem);
             }
             detailsListAdapter = new DetailsListAdapter(this, myDetailsArray, getResources());
-            ListView listView = (ListView) findViewById(R.id.details_list);
+            ExpandableListView listView = (ExpandableListView) findViewById(R.id.details_list);
             listView.setAdapter(detailsListAdapter);
 
         } catch (JSONException e) {
@@ -163,6 +225,130 @@ public class Detail_City_Activity extends MyDrawerLayoutActivity {
         }
 
     }
+
+
+
+
+
+
+
+    private static class GroupItem {
+        String title;
+        List<ChildItem> items = new ArrayList<ChildItem>();
+    }
+
+    private static class ChildItem {
+        String title;
+        String hint;
+    }
+
+    private static class ChildHolder {
+        TextView title;
+        TextView hint;
+    }
+
+    private static class GroupHolder {
+        TextView title;
+    }
+
+    /**
+     * Adapter for our list of {@link GroupItem}s.
+     */
+    private class ExampleAdapter extends AnimatedExpandableListView.AnimatedExpandableListAdapter {
+        private LayoutInflater inflater;
+
+        private List<GroupItem> items;
+
+        public ExampleAdapter(Context context) {
+            inflater = LayoutInflater.from(context);
+        }
+
+        public void setData(List<GroupItem> items) {
+            this.items = items;
+        }
+
+        @Override
+        public ChildItem getChild(int groupPosition, int childPosition) {
+            return items.get(groupPosition).items.get(childPosition);
+        }
+
+        @Override
+        public long getChildId(int groupPosition, int childPosition) {
+            return childPosition;
+        }
+
+        @Override
+        public View getRealChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+            ChildHolder holder;
+            ChildItem item = getChild(groupPosition, childPosition);
+            if (convertView == null) {
+                holder = new ChildHolder();
+                convertView = inflater.inflate(R.layout.list_item, parent, false);
+                holder.title = (TextView) convertView.findViewById(R.id.textTitle);
+                holder.hint = (TextView) convertView.findViewById(R.id.textHint);
+                convertView.setTag(holder);
+            } else {
+                holder = (ChildHolder) convertView.getTag();
+            }
+
+            holder.title.setText(item.title);
+            holder.hint.setText(item.hint);
+
+            return convertView;
+        }
+
+        @Override
+        public int getRealChildrenCount(int groupPosition) {
+            return items.get(groupPosition).items.size();
+        }
+
+        @Override
+        public GroupItem getGroup(int groupPosition) {
+            return items.get(groupPosition);
+        }
+
+        @Override
+        public int getGroupCount() {
+            return items.size();
+        }
+
+        @Override
+        public long getGroupId(int groupPosition) {
+            return groupPosition;
+        }
+
+        @Override
+        public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+            GroupHolder holder;
+            GroupItem item = getGroup(groupPosition);
+            if (convertView == null) {
+                holder = new GroupHolder();
+                convertView = inflater.inflate(R.layout.city_details_item, parent, false);
+                holder.title = (TextView) convertView.findViewById(R.id.details_item_title);
+                convertView.setTag(holder);
+            } else {
+                holder = (GroupHolder) convertView.getTag();
+            }
+
+            holder.title.setText(item.title);
+
+            return convertView;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
+        @Override
+        public boolean isChildSelectable(int arg0, int arg1) {
+            return true;
+        }
+
+    }
+
+
+
 
 
 }
