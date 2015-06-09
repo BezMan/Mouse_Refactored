df2bed42f3f19c93f5a8e8eb776bc2270239a1a6
Bez <bez@comrax.com>
2015-06-09 10:38:12 +0300

    got a sticky header working example.

--- app/src/main/java/com/comrax/mouseappandroid/activities_N_fragments/Open_Details_header_N_list_Activity.java
+++ app/src/main/java/com/comrax/mouseappandroid/activities_N_fragments/Open_Details_header_N_list_Activity.java
@@ -1,24 +1,41 @@
 package com.comrax.mouseappandroid.activities_N_fragments;
 
 import android.os.Bundle;
-import android.widget.Toast;
+import android.support.v4.app.Fragment;
 
 import com.comrax.mouseappandroid.R;
 
 /**
  * Created by bez on 07/06/2015.
  */
-public class Open_Details_header_N_list_Activity extends MyDrawerLayoutActivity {
+public class Open_Details_header_N_list_Activity extends MyDrawerLayoutActivity{
 
-    @Override
-    protected int getLayoutResourceId() {
-        return R.layout.open_details_full_header;
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Toast.makeText(getApplicationContext(), "agrt", Toast.LENGTH_SHORT).show();
+        if (savedInstanceState == null) {
+            loadFragment(new SimpleStikkyFragment());
+        }
+
+    }
+
+
+
+    public void loadFragment(final Fragment fragment) {
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.layout_container, fragment, fragment.getClass().getName())
+                .addToBackStack(fragment.getClass().getName())
+                .commit();
+
+    }
+
+    @Override
+    protected int getLayoutResourceId() {
+        return R.layout.open_details_full_header;
     }
 }
+
---
+++ app/src/main/java/com/comrax/mouseappandroid/activities_N_fragments/SimpleStikkyFragment.java
@@ -0,0 +1,76 @@
+package com.comrax.mouseappandroid.activities_N_fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.comrax.mouseappandroid.R;
+
+import it.carlom.stikkyheader.core.StikkyHeaderBuilder;
+
+public class SimpleStikkyFragment extends Fragment {
+
+    private ListView mListView;
+    private Button btn;
+
+    public SimpleStikkyFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_simplelistview, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mListView = (ListView) view.findViewById(R.id.listview);
+        btn = (Button)view.findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity().getApplicationContext(), "great", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        StikkyHeaderBuilder.stickTo(mListView)
+            .setHeader(R.id.header, (ViewGroup) getView())
+                .minHeightHeader(100)
+            .build();
+
+
+        populateListView();
+
+    }
+
+    private void populateListView() {
+
+        String[] elements = new String[500];
+        for (int i = 0; i < elements.length; i++) {
+            elements[i] = "row " + i;
+        }
+
+        mListView.setAdapter(new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, elements));
+
+    }
+
+}
