91e615d761f4a3003bb0c9f69d378ce6dcc68e20
Bez <bez@comrax.com>
2015-06-14 17:28:33 +0300

    screening works!

--- app/src/main/java/com/comrax/mouseappandroid/activities_N_fragments/SimpleStikkyFragment.java
+++ app/src/main/java/com/comrax/mouseappandroid/activities_N_fragments/SimpleStikkyFragment.java
@@ -16,7 +16,6 @@ import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.comrax.mouseappandroid.App;
 import com.comrax.mouseappandroid.R;
@@ -91,21 +90,20 @@ public class SimpleStikkyFragment extends Fragment {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 // find which radio button is selected
-                if(checkedId == R.id.btn1) {
-                    Toast.makeText(getActivity().getApplicationContext(), "choice: Silent",
-                            Toast.LENGTH_SHORT).show();
-                } else if(checkedId == R.id.btn2) {
-                    Toast.makeText(getActivity().getApplicationContext(), "choice: Sound",
-                            Toast.LENGTH_SHORT).show();
+                if (checkedId == R.id.btn1) {
+                    screenListView(0);
+                } else if (checkedId == R.id.btn2) {
+                    screenListView(3);
+                } else if (checkedId == R.id.btn3) {
+                    screenListView(2);
                 } else {
-                    Toast.makeText(getActivity().getApplicationContext(), "choice: Vibration",
-                            Toast.LENGTH_SHORT).show();
+                    screenListView(1);
                 }
             }
 
         });
 
-        resultsTxtView = (TextView)getActivity().findViewById(R.id.txtResultsCount);
+        resultsTxtView = (TextView) getActivity().findViewById(R.id.txtResultsCount);
 
         mListView = (ListView) getActivity().findViewById(R.id.listview);
         mLocationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
@@ -134,26 +132,49 @@ public class SimpleStikkyFragment extends Fragment {
 
     }
 
+
+    private void screenListView(int price) {
+
+        if (price == 0) {
+
+            adapter = new CustomAdapter(getActivity(), customListViewValuesArr, getResources());
+            mListView.setAdapter(adapter);
+
+        } else {
+
+            ArrayList<ListModel> screenedListViewValuesArr = new ArrayList<>();
+
+            for (int i = 0; i < customListViewValuesArr.size(); i++) {
+                if (customListViewValuesArr.get(i).getPrice() == price) {
+                    screenedListViewValuesArr.add(customListViewValuesArr.get(i));
+                }
+            }
+            adapter = new CustomAdapter(getActivity(), screenedListViewValuesArr, getResources());
+            mListView.setAdapter(adapter);
+        }
+    }
+
+
     private void populateListView() {
 
         String cityId = App.getInstance().getCityId();
         String boneId = App.getInstance().getBoneId();
         Cursor cursor = new DBTools(getActivity()).getCurrentCityPlacesTable(DBConstants.PLACE_TABLE_NAME, DBConstants.cityId, cityId, DBConstants.boneId, boneId);
 
-        resultsTxtView.setText("התקבלו "+ cursor.getCount() + " תוצאות");
+        resultsTxtView.setText("התקבלו " + cursor.getCount() + " תוצאות");
 
-            for (int i = 0; i < cursor.getCount(); cursor.moveToNext(), i++) {
-                ListModel lm = new ListModel();
+        for (int i = 0; i < cursor.getCount(); cursor.moveToNext(), i++) {
+            ListModel lm = new ListModel();
 
-                lm.setTitleA(cursor.getString(cursor.getColumnIndex(DBConstants.name)));
-                lm.setTitleB(cursor.getString(cursor.getColumnIndex(DBConstants.hebrewName)));
-                lm.setTitleC(cursor.getString(cursor.getColumnIndex(DBConstants.type)));
+            lm.setTitleA(cursor.getString(cursor.getColumnIndex(DBConstants.name)));
+            lm.setTitleB(cursor.getString(cursor.getColumnIndex(DBConstants.hebrewName)));
+            lm.setTitleC(cursor.getString(cursor.getColumnIndex(DBConstants.type)));
 
 
-                lm.setPrice(cursor.getInt(cursor.getColumnIndex(DBConstants.price)));
-                lm.setAddress(cursor.getString(cursor.getColumnIndex(DBConstants.address)));
+            lm.setPrice(cursor.getInt(cursor.getColumnIndex(DBConstants.price)));
+            lm.setAddress(cursor.getString(cursor.getColumnIndex(DBConstants.address)));
 
-                lm.setRating(cursor.getFloat(cursor.getColumnIndex(DBConstants.rating)));
+            lm.setRating(cursor.getFloat(cursor.getColumnIndex(DBConstants.rating)));
 
 //                lm.setLatitude();
 //                lm.setLongitude();
@@ -163,18 +184,18 @@ public class SimpleStikkyFragment extends Fragment {
 //If results has length 2 or greater, the initial bearing is stored in results[1].
 //If results has length 3 or greater, the final bearing is stored in results[2].
 
-                float[] results = new float[1];
+            float[] results = new float[1];
 
-                Location.distanceBetween(lat, lon,
-                        cursor.getDouble(cursor.getColumnIndex(DBConstants.centerCoordinateLat)), cursor.getDouble(cursor.getColumnIndex(DBConstants.centerCoordinateLon)), results);
+            Location.distanceBetween(lat, lon,
+                    cursor.getDouble(cursor.getColumnIndex(DBConstants.centerCoordinateLat)), cursor.getDouble(cursor.getColumnIndex(DBConstants.centerCoordinateLon)), results);
 
-                lm.setDistance(results[0]);
+            lm.setDistance(results[0]);
             //my lon and lat are 0//
-                /////////
+            /////////
 
-                customListViewValuesArr.add(lm);
+            customListViewValuesArr.add(lm);
 
-            }
+        }
 
 
     }
