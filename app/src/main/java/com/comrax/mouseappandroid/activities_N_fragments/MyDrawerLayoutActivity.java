6b82bd2fb0b94fa5a1c67b6df3bd324ca036cfe0
Bez <bez@comrax.com>
2015-07-01 15:54:10 +0300

    start of cleaning code.

--- app/src/main/java/com/comrax/mouseappandroid/activities_N_fragments/MyDrawerLayoutActivity.java
+++ app/src/main/java/com/comrax/mouseappandroid/activities_N_fragments/MyDrawerLayoutActivity.java
@@ -2,24 +2,26 @@ package com.comrax.mouseappandroid.activities_N_fragments;
 
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.comrax.mouseappandroid.app.App;
 import com.comrax.mouseappandroid.R;
-import com.comrax.mouseappandroid.favorites.SectionDemoActivity;
 import com.comrax.mouseappandroid.adapters.CustomGlobalNavDrawerAdapter;
-import com.comrax.mouseappandroid.model.DrawerModel;
+import com.comrax.mouseappandroid.app.App;
 import com.comrax.mouseappandroid.app.GlobalVars;
+import com.comrax.mouseappandroid.favorites.SectionDemoActivity;
+import com.comrax.mouseappandroid.model.DrawerModel;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -137,77 +139,101 @@ public abstract class MyDrawerLayoutActivity extends AppCompatActivity {
             mDrawerLayout.closeDrawers();
     }
 
+
     public void onNavDrawerItemClick(int mPosition) {
-        DrawerModel tempValues = customDrawerItemsArr.get(mPosition);
-//        Toast.makeText(this, "" + tempValues.getBtnImage() + " \n" + mPosition + " \n", Toast.LENGTH_LONG).show();
-
-
         TextView barTitleTextView = (TextView) findViewById(R.id.title_text);
         App.getInstance().setAppBarTitle(barTitleTextView.getText().toString());
 
 
         if (mPosition == 0) {
-            Intent cityIntent = new Intent(this, MainListActivity.class);
-            cityIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            startActivity(cityIntent);
+            allCities();
 
         } else if (mPosition == 1) {
-
-
-            Fragment staticPageFragment = getSupportFragmentManager().findFragmentByTag(STATIC_PAGE_TAG);
-
-            //if frag open, just close it...
-            if (this instanceof SectionDemoActivity && staticPageFragment!=null && staticPageFragment.isVisible()) { //if in static page:
-                getSupportFragmentManager()
-                        .beginTransaction()
-                        .remove(staticPageFragment)
-//                    .addToBackStack(fragment.getClass().getName())
-                        .commit();
-            }
-            else {
-                Intent intent = new Intent(this, SectionDemoActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-                startActivity(intent);
-            }
+            openFavorites();
 
         } else if (mPosition == 2) {
-        } else if (mPosition == 3) {
-        } else {
-            for (int i = 0; i < GlobalVars.staticPagesArray.length(); i++) {
-                try {
+            emailShareApp();
 
-                    JSONObject jsonObject = GlobalVars.staticPagesArray.getJSONObject(i);
-                    if ((mPosition == 4 && jsonObject.getString("id").equals("3")) || (mPosition == 5 && jsonObject.getString("id").equals("2")) || (mPosition == 6 && jsonObject.getString("id").equals("1"))) {
-                        nextActivity(jsonObject, tempValues.getBtnTitle());
-                        break;
-                    }
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
+        } else if (mPosition == 3) {
+            rateApp();
+
+        } else {
+            openStaticPage(mPosition);
         }
         mDrawerLayout.closeDrawers();
     }
 
-    private void nextActivity(JSONObject jsonObject, String barTitle) {
-//        Intent staticPageIntent = new Intent(this, StaticPageActivity.class);
-//        staticPageIntent.putExtra("data", jsonObject.toString());
-//        staticPageIntent.putExtra("barTitle", barTitle);
-//        startActivity(staticPageIntent);
-//        if(this instanceof StaticPageActivity){
-//            finish();
-//        }
 
-        Bundle bundle = new Bundle();
-        bundle.putString("data", jsonObject.toString());
-        bundle.putString("barTitle", barTitle);
 
-        Fragment staticPageFragment = new StaticPageFragment();
 
-        staticPageFragment.setArguments(bundle);
-        loadFragment(staticPageFragment, STATIC_PAGE_TAG);
+    private void allCities() {
+        Intent cityIntent = new Intent(this, MainListActivity.class);
+        cityIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(cityIntent);
+    }
+
+
+
+    private void openFavorites() {
+        Fragment staticPageFragment = getSupportFragmentManager().findFragmentByTag(STATIC_PAGE_TAG);
+
+        //if frag open, just close it...
+        if (this instanceof SectionDemoActivity && staticPageFragment!=null && staticPageFragment.isVisible()) { //if in static page:
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .remove(staticPageFragment)
+//                    .addToBackStack(fragment.getClass().getName())
+                    .commit();
+        }
+        else {
+            Intent intent = new Intent(this, SectionDemoActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+            startActivity(intent);
+        }
 
     }
+
+
+    private void emailShareApp() {
+        String websiteAddress = "http://www.mouse.co.il/";
+        Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                "mailto", "", null));
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "חבר/ה שלך הוריד/ה את אפליקציית עכבר עולם.");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, Html.fromHtml("הורד את האפליקציה באמצעות הלינק הבא: \n\n  <a href=\"" + websiteAddress + "\"> " + "להורדה" + "</a>"));
+        startActivity(Intent.createChooser(emailIntent, "Send email..."));
+    }
+
+
+
+    private void rateApp() {
+    }
+
+
+    private void openStaticPage(int mPosition) {
+        DrawerModel tempValues = customDrawerItemsArr.get(mPosition);
+        for (int i = 0; i < GlobalVars.staticPagesArray.length(); i++) {
+            try {
+
+                JSONObject jsonObject = GlobalVars.staticPagesArray.getJSONObject(i);
+                if ((mPosition == 4 && jsonObject.getString("id").equals("3")) || (mPosition == 5 && jsonObject.getString("id").equals("2")) || (mPosition == 6 && jsonObject.getString("id").equals("1"))) {
+
+                    Bundle bundle = new Bundle();
+                    bundle.putString("data", jsonObject.toString());
+                    bundle.putString("barTitle", tempValues.getBtnTitle());
+
+                    Fragment staticPageFragment = new StaticPageFragment();
+                    staticPageFragment.setArguments(bundle);
+                    loadFragment(staticPageFragment, STATIC_PAGE_TAG);
+
+                    break;
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }}
+
+
+
 
     public void loadFragment(final Fragment fragment, String fragTag) {
 
